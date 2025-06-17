c2aad1ccbcd049b09b33cee246a36ce62b585b29
Elliot Rosen <elliotrosen2@gmail.com>
2025-06-17 18:52:14 -0400

    Updating landing page messaging

--- components/LandingPage.tsx
+++ components/LandingPage.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+// Updated landing page messaging
 import { useState } from "react"
 import { Button } from "@/components/ui/button"
 import { SignInButton, SignUpButton } from "@clerk/nextjs"
